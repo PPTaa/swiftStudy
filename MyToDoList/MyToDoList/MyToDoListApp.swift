2f34548232197665e9ddfb9d9e9b1a331e303bbd
pptaa <chobo05@naver.com>
2020-12-16 11:11:25 +0900

    ignore test

---
+++ MyToDoList/MyToDoList/MyToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoListApp.swift
+//  MyToDoList
+//
+//  Created by leejungchul on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
